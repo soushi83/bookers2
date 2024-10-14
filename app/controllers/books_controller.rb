2cf294322d93882bf8f715ca9d63c93f42c41e93
EC2 Default User <ec2-user@ip-172-31-15-138.ap-northeast-1.compute.internal>
2024-10-14 03:03:51 +0000

    make layout

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:edit, :update, :show, :destroy]
+
   def index
     @books = Book.all
     @new_book = Book.new
@@ -16,14 +18,35 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
+    @new_book = Book.new
   end
 
   def edit
+    @book
+  end
+
+  def update
+    if @book.update(book_params)
+      redirect_to @book, notice: 'Book was successefully update.'
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @book.destroy
+    redirect_to books_path, notice: 'Book was successfully deleted.'
   end
 
   private
 
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get "/homes/about" => "homes#about", as: "about"
   devise_for :users
 
-  resources :books, only: [:index, :create, :show, :edit]
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
   resources :users, only: [:index, :show, :edit]
 
 end
